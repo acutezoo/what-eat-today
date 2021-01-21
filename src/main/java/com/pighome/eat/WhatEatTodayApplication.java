ef672f19a8768a4ec459e138700f2068be492574
zhangyuanyuan <zhangyuanyuan@nnuo.com>
2021-01-21 13:25:15 +0800

    init

---
+++ src/main/java/com/pighome/eat/WhatEatTodayApplication.java
@@ -0,0 +1,13 @@
+package com.pighome.eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatEatTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatEatTodayApplication.class, args);
+	}
+
+}
